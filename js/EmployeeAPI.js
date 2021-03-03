a467575a88dc5fecf1fcd25eca68508f27166ab6
Sadra Khosravi <sadrakh@outlook.com>
2021-03-02 23:52:49 -0800

    Added click event for each employee card

--- js/EmployeeAPI.js
+++ js/EmployeeAPI.js
@@ -1,8 +1,13 @@
 class EmployeeAPI {
   constructor(url) {
     this.url = url;
+    this.employees = null;
   }
 
+  /**
+   * Gets the API data from the given url.
+   * @return {Promise} - Promise data returned by the API.
+   */
   async getData() {
     try {
       const employees = await fetch(this.url);
@@ -12,18 +17,22 @@ class EmployeeAPI {
     }
   }
 
-  outputEmployees() {
-    this.getData().then(data => this.generateHTML(data));
+  generateEmployees(data) {
+    return data.results;
   }
 
+  /**
+   * Outputs the selected data from the API to the screen.
+   * @param {Array} data - An array of objects containing user's data.
+   * @return {Array} - An array of employee objects.
+   */
   generateHTML(data) {
-    const employees = data.results;
-    console.log(employees);
-    employees.forEach(employee => {
+    this.employees = data.results;
+    this.employees.forEach(employee => {
       let employeeHTML = `
       <div class="card">
           <div class="card-img-container">
-              <img class="card-img" src="${employee.picture.large}" alt="profile picture">
+              <img class="card-img" src="${employee.picture.large}" alt="${employee.name.first} ${employee.name.last}'s profile picture">
           </div>
           <div class="card-info-container">
               <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
@@ -34,5 +43,14 @@ class EmployeeAPI {
     `;
       employeeGallery.insertAdjacentHTML('beforeend', employeeHTML);
     });
+  }
+
+  modalOutput() {}
+
+  /**
+   * Calls the "generateHTML" method on the response promise returned by the API
+   */
+  outputEmployees() {
+    this.getData().then(data => this.generateHTML(data));
   }
 }
